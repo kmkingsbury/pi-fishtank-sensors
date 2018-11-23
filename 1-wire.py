9133cfc5c4cd0716f861ba81a9d8d6f6c2b17a25
Kevin Kingsbury <kkingsbury@gmail.com>
2018-11-23 11:59:09 -0600

    Added DB Creds to password file

--- 1-wire.py
+++ 1-wire.py
@@ -1,6 +1,8 @@
 import os
 import glob
 import time
+import logging
+
  
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
@@ -8,13 +10,20 @@ os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
- 
+
+#logging.disable(logging.DEBUG)
+
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+
+dbuser = ''
+dbpass = ''
+
 def read_temp_raw():
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
- 
+
 def read_temp():
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
@@ -26,7 +35,17 @@ def read_temp():
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
         return temp_c, temp_f
+
+with open('passwordfile.txt') as f:
+    credentials = [x.strip().split(':') for x in f.readlines()]
+
+for username,password in credentials:
+   dbuser = username
+   dbpass = password
+   break 
 	
+
+logging.debug("DB User/pass" + dbuser + "/" + dbpass )
 while True:
 	print("Temp (C,F): " + str(read_temp()))	
 	time.sleep(1)
